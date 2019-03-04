f97857ab0ecb4fa50a32a290cbf2ace1cf19e804
Bharat Medasani <mbkumar@gmail.com>
2019-03-04 11:07:06 -0500

    Interaction effects added to the SpeciesThermo

---
+++ include/cantera/thermo/MultiSpeciesInterThermo.h
@@ -0,0 +1,124 @@
+/**
+ * @file MultiSpeciesInterThermo.h
+ *  Header for a general species thermodynamic property manager for a phase where 
+ *  species have interactions (see
+ * \link Cantera::MultiSpeciesInterThermo MultiSpeciesInterThermo\endlink).
+ */
+
+// This file is part of Cantera. See License.txt in the top-level directory or
+// at http://www.cantera.org/license.txt for license and copyright information.
+
+#ifndef CT_MULTISPECIESINTERTHERMO_H
+#define CT_MULTISPECIESINTERTHERMO_H
+
+//#include "MultiSpeciesThermo.h"
+#include <memory>
+#include <vector>
+#include <utility>
+#include <map>
+#include "cantera/numerics/eigen_dense.h"
+
+namespace Cantera
+{
+
+class LateralInteraction;
+
+//! A species thermodynamic property manager for a phase that explicitly accounts 
+//! for interactions between species.
+/*!
+ * This is a derived class of MultiSpeciesThermo, which is a general manager that 
+ * can handle a wide variety of species * thermodynamic polynomials for individual 
+ * species and compute their * nondimensional, reference-state thermodynamic 
+ * properties (i.e. as a function * of temperature only). Whereas MultiSpeciesThermo
+ * does not account for interaction between species, MultiSpeciesInteractionThermo
+ * does based on the interaction strength defined. 
+ *
+ * Developed to account for lateral interactions between surface species
+ *
+ * The ThermoPhase object relies on MultiSpeciesInterThermo to calculate the
+ * contributions of interactions to the thermodynamic properties of the 
+ * reference state for all of the species in the
+ * phase, for a range of temperatures. 
+ *
+ * The most important member function for the MultiSpeciesInterThermo class is the
+ * member function MultiSpeciesInterThermo::update(). The function calculates the
+ * values of Cp/R, H/RT, and S/R coming from lateral interactions for all of the 
+ * surface species at once at the specified coverage.
+ *
+ * Usually, all of the species in a interacting surface phase are installed into a
+ * MultiSpeciesInterThermo object. However, there is no requirement that a
+ * MultiSpeciesInterThermo object handles all of the species in a phase. The member
+ * function
+ * \link MultiSpeciesInterThermo::install_STIT() install_STIT()\endlink
+ * is called to install each species into the MultiSpeciesSurfThermo object.
+ *
+ * @ingroup spthermo
+ */
+class MultiSpeciesInterThermo
+{
+public:
+    //! Constructor
+    MultiSpeciesInterThermo();
+
+    // MultiSpeciesInterThermo objects are not copyable or assignable
+    MultiSpeciesInterThermo(const MultiSpeciesInterThermo& b) = delete;
+    MultiSpeciesInterThermo& operator=(const MultiSpeciesInterThermo& b) = delete;
+    virtual ~MultiSpeciesInterThermo() {}
+
+    //! Install interaction parameterization of species
+    /*!
+     *  @param index           Index of the species being installed
+     *  @param interParameters interaction parameters of the species
+     */
+    void addInteraction(std::shared_ptr<LateralInteraction> interaction);
+
+    //! Build the map between the indices of species and interactions
+    /*!
+     *  @param species   Ordered species strings in a phase 
+     */
+    void buildSpeciesInterMap(std::vector<std::string> species);
+
+    //! Compute the coverage dependent lateral interactions for all species.
+    /*!
+     * Given temperature T in K, this method updates the values of -
+     * enthalpy for the given coverages, Pref of each of the standard states.
+     *
+     * @param T         Temperature (Kelvin)
+     * @param coverages Coverages (vector of floats)
+     * @param h_RT      Vector of Dimensionless enthalpies. (length m_kk).
+     */
+    void update(doublereal T, doublereal* coverages, doublereal* h_rt);
+
+    //! This utility function reports back the interaction
+    //! parameters for the species with index number *index*.
+    /*!
+     * @param index         Species index
+     * @param type          Integer type of the Interaction type.
+     * @param c             Vector of interaction coefficients.
+     * @param covIntercept1 output -  1st intercept of coverage. One means doesn't exist.
+     * @param covIntercept2 output -  2nd intercept of coverage. One means doesn't exist.
+     */
+    /*void reportInteractionParams(size_t index, int& type,
+                                 doublereal* const c,
+                                 doublereal& covIntercept1,
+                                 doublereal& covIntercept2) const;
+    */
+    //! Check if data for all species (0 through nSpecies-1) has been installed.
+    bool ready(size_t nSpecies);
+
+protected:
+
+    //! Store the interaction objects to obtain the interactions strengths
+    std::vector<std::shared_ptr<LateralInteraction> > m_interactions;
+    //! Map the interaction index to species indices
+    std::map< std::pair<int, int>, int> m_specie_inter_map;
+    //! Interaction strength parameters (matrix) stored in vector form
+    Eigen::MatrixXd m_int_strengths;
+    //! Vector of coverages 
+    Eigen::VectorXd m_coverages;
+
+};
+
+}
+
+#endif
---
+++ src/thermo/MultiSpeciesInterThermo.cpp
@@ -0,0 +1,113 @@
+/**
+ *  @file MultiSpeciesInterThermo.cpp
+ *  Declarations for a thermodynamic property manager for multiple species
+ *  in a phase (see \ref spthermo and
+ * \link Cantera::MultiSpeciesInterThermo MultiSpeciesInterThermo\endlink).
+ */
+
+// This file is part of Cantera. See License.txt in the top-level directory or
+// at http://www.cantera.org/license.txt for license and copyright information.
+
+#include "cantera/thermo/MultiSpeciesInterThermo.h"
+#include "cantera/thermo/LateralInteraction.h"
+//#include "cantera/thermo/MultiSpeciesThermo.h"
+//#include "cantera/thermo/SpeciesThermoFactory.h"
+#include "cantera/base/stringUtils.h"
+#include "cantera/base/utilities.h"
+#include "cantera/base/ctexceptions.h"
+#include "cantera/numerics/eigen_dense.h"
+
+namespace Cantera
+{
+
+MultiSpeciesInterThermo::MultiSpeciesInterThermo() 
+{
+}
+
+void MultiSpeciesInterThermo::addInteraction(
+        shared_ptr<LateralInteraction> interaction)
+{
+    m_interactions.push_back(interaction); 
+}
+
+int get_index(std::vector<std::string> species, std::string name) {
+    auto it = std::find(species.begin(), species.end(), name);
+    if (it == species.end())
+    {
+        // name not in vector
+        //throw CanteraError(
+                //"Name of species in interaction not found in supplied species");
+    } else
+    {
+        return std::distance(species.begin(), it);
+    }
+
+}
+void MultiSpeciesInterThermo::buildSpeciesInterMap(std::vector<std::string> species)
+{
+    auto cnt = 0;
+    for (auto inter : m_interactions) {
+        std::string sp1 = inter->species1Name();
+        std::string sp2 = inter->species2Name();
+
+        auto ind1 = get_index(species, sp1);
+        auto ind2 = get_index(species, sp2);
+        m_specie_inter_map[std::make_pair(ind1, ind2)] = cnt;
+        cnt++;
+    }
+
+    if (m_int_strengths.size() == 0) { // Initialize to zero values. 
+        m_int_strengths = Eigen::MatrixXd.Zero(species.size(), species.size())
+    }
+
+    if (m_coverages.size() == 0) { // Not yet initialized, initialize to zero values
+        m_coverages = Eigen::VectorXd.Zero(species.size())
+    }
+
+}
+
+void MultiSpeciesInterThermo::update(doublereal t, doublereal* coverages, 
+                                     doublereal* h_RT) 
+{
+    
+
+    for (auto i = 0; i <  m_coverages.size(); i++){
+        if (coverages[i] != m_coverages[i]){
+            m_coverages(i) = coverages[i];
+            for (auto j=0; j < m_coverages.size(); j++){
+                auto inter = m_interactions[m_specie_inter_map[std::make_pair(j, i)]];
+                m_int_strengths(j,i) = inter->strength(m_coverages[i]);
+            }
+        }
+    }
+    *h_RT = (m_int_strengths * m_coverages).sum();
+    //return interaction;
+}
+
+/*
+void MultiSpeciesInterThermo::reportInteractionParams(size_t index, int& type,
+        doublereal* const c, doublereal& covIntercept1, doublereal& covIntercept2,
+        doublereal& refPressure_) const
+{
+    const SpeciesThermoInterpType* sp = provideSTIT(index);
+    size_t n;
+    if (sp) {
+        sp->reportParameters(n, type, minTemp_, maxTemp_,
+                             refPressure_, c);
+    } else {
+        type = -1;
+    }
+}
+*/
+
+
+
+bool MultiSpeciesInterThermo::ready(size_t nSpecies) {
+    if (m_interactions.size() < nSpecies * nSpecies) {
+        return false;
+    }
+    return true;
+}
+
+
+}
