bc09eb47211e04ca0db3674499507242abc08c6d
Bharat Medasani <mbkumar@gmail.com>
2019-03-06 11:02:58 -0500

    InterfaceLatInt interface added

--- src/thermo/LateralInteraction.cpp
+++ src/thermo/LateralInteraction.cpp
@@ -8,6 +8,10 @@
 #include "cantera/base/ctml.h"
 #include <iostream>
 #include <limits>
+#include <vector>
+#include <string>
+#include <utility>
+
 
 namespace Cantera {
 
@@ -15,14 +19,16 @@ LateralInteraction::LateralInteraction()
 {
 }
 
-/*
-LateralInteraction::LateralInteraction(const Species* const species, 
-                                       const double* const strengths,   
-                                       const double* const intercepts)
-    : m_spcies(species), m_strengths(strengths), m_intercepts(intercepts)
+
+LateralInteraction::LateralInteraction(std::string species1, std::string species2,
+                                       const vector_fp& strengths,   
+                                       const vector_fp& intercepts, 
+                                       std::string name) :
+    m_strengths(strengths), m_cov_thresholds(intercepts), m_id(name)
 {
+    m_species = make_pair(species1, species2);
 }
-*/
+
 
 LateralInteraction::~LateralInteraction()
 {
@@ -37,35 +43,77 @@ bool LateralInteraction::validate()
 }
 
 
-std::string LateralInteraction::species1Name() { return m_species.first->name; }
+std::string LateralInteraction::species1Name() { 
+    //return m_species.first->name; 
+    return m_species.first; 
+}
 
-std::string LateralInteraction::species2Name() { return m_species.second->name; }
+std::string LateralInteraction::species2Name() { 
+    //return m_species.second->name; 
+    return m_species.second; 
+}
+
+double LateralInteraction::strength(const double coverage) const {
+    /*
+    if (coverage > 1) {
+        throw CanteraError ("Coverage cannot be greater than 1");
+    }
+    else {
+        if (coverage < 0) {
+            throw CanteraError ("Coverage cannot be less than 0");
+        }
+    }*/
+
+    doublereal val = 0.0;
+    for (auto i=0; i < m_strengths.size(); i++) {
+        auto cov_low_thr = m_cov_thresholds[i];
+        auto cov_up_thr = m_cov_thresholds[i+1];
+        if (cov_up_thr < coverage) {
+            val += (cov_up_thr - cov_low_thr) * m_strengths[i];
+        } 
+        else {
+            val += (coverage - cov_low_thr) * m_strengths[i];
+            break;
+        }
+    }
+
+    return  val;
+}
+
 
 
 shared_ptr<LateralInteraction> newLateralInteraction(const XML_Node& interaction_node)
 {
-    if (interaction_node.hasAttrib("id")){
-        std::string id = interaction_node["id"];
-    }
+    //if (interaction_node.hasAttrib("id")){
+    std::string id = interaction_node["id"];
+    //}
     const XML_Node& sp_array = interaction_node.child("speciesArray");
-    XML_Node* db = get_XML_Node(sp_array["datasrc"], &interaction_node.root());
+    std::vector<std::string> species;
+    getStringArray(sp_array, species);
+    if (species.size() != 2)
+        throw CanteraError("Cantera::newLateralInteraction", 
+                "The size of the species array: '{}' is different from 2",  
+                species.size());
+                           //+ sp_array["datasrc"]);
+    //XML_Node* db = get_XML_Node(sp_array["datasrc"], &interaction_node.root());
     /*if (db == 0) {
         throw CanteraError(" Can not find the XML node for species databases: ");
                            //+ sp_array["datasrc"]);
     }*/
     
     std::vector<XML_Node*> fas = interaction_node.getChildren("floatArray");
-    vector_fp m_strengths, m_cov_thresholds;
+    vector_fp strengths, cov_thresholds;
     for (auto fa: fas){
         if (fa->name() == "strength")
-            getFloatArray(*fa, m_strengths);//, nodeName = "strength")
+            getFloatArray(*fa, strengths);//, nodeName = "strength")
         if (fa->name() == "coverage_threshold")
-            getFloatArray(*fa, m_cov_thresholds);//, nodeName = "coverage_threshold")
+            getFloatArray(*fa, cov_thresholds);//, nodeName = "coverage_threshold")
     }
 
     
-    //auto interaction = make_shared<LateralInteraction>(species, strengths, intercepts);
-    auto interaction = make_shared<LateralInteraction>();
+    auto interaction = make_shared<LateralInteraction>(species[0], species[1], 
+                                                       strengths, cov_thresholds, id);
+    //auto interaction = make_shared<LateralInteraction>();
 
     return interaction;
 }
