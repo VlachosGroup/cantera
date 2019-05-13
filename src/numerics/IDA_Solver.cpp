5fe9d632d9adb39f686d415d8af84a930b9b7941
Bharat Medasani <mbkumar@gmail.com>
2019-05-13 18:58:19 -0400

    Deleting null IDA memory block eliminated

--- src/numerics/IDA_Solver.cpp
+++ src/numerics/IDA_Solver.cpp
@@ -419,8 +419,8 @@ void IDA_Solver::init(doublereal t0)
         long int N = m_neq;
         int flag;
         #if CT_SUNDIALS_VERSION >= 30
-            SUNLinSolFree((SUNLinearSolver) m_linsol);
-            SUNMatDestroy((SUNMatrix) m_linsol_matrix);
+            //SUNLinSolFree((SUNLinearSolver) m_linsol);
+            //SUNMatDestroy((SUNMatrix) m_linsol_matrix);
             m_linsol_matrix = SUNDenseMatrix(N, N);
             if (m_linsol_matrix == nullptr) {
                 throw CanteraError("IDA_Solver::init",
@@ -444,8 +444,8 @@ void IDA_Solver::init(doublereal t0)
         long int nu = m_mupper;
         long int nl = m_mlower;
         #if CT_SUNDIALS_VERSION >= 30
-            SUNLinSolFree((SUNLinearSolver) m_linsol);
-            SUNMatDestroy((SUNMatrix) m_linsol_matrix);
+            //SUNLinSolFree((SUNLinearSolver) m_linsol);
+            //SUNMatDestroy((SUNMatrix) m_linsol_matrix);
             m_linsol_matrix = SUNBandMatrix(N, nu, nl, nu+nl);
             if (m_linsol_matrix == nullptr) {
                 throw CanteraError("IDA_Solver::init",
